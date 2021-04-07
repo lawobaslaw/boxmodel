7b010bc82cab6401a3e033d7e5180a84a6a03e74
lawobaslaw <lawobaslaw@gmail.com>
2021-04-07 08:01:54 -0500

    java claswork

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var num1 = Number(prompt("Enter Number 1"));
+var operator = prompt("Enter Operator");
+var num2 = Number(prompt("Enter Number 2"));
+
+
+if (operator == "+"){
+   var result = num1 + num2
+   alert(`The addition of ${num1} and ${num2} is ${result}`)
+}
+else if (operator == "-"){
+    var result = num1 - num2
+    alert(`The subtraction of ${num1} and ${num2} is ${result}`)
+ }
+
+
+ else if (operator == "*"){
+    var result = num1 * num2
+    alert(`The multiplication of ${num1} and ${num2} is ${result}`)
+ }
+ else{alert(` failed`)}
